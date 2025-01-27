d1e04e3ac960c1f53c55de491220b7c2a07cc15b
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-01-27 11:35:12 +0200

    change target date

--- src/Params.js
+++ src/Params.js
@@ -2,6 +2,6 @@ export default class Params {
   static keyPush = '181814a0-008e-49b1-bcfb-8e77183d43fc'; // ключ OneSignal
   static appID = '6738984992'; // ID додатку в сторі
   static keyApps = 'BX5RmT4UeiWBs7XFeqzzDM'; // ключ Appsflyer
-  static targetDate = '2025-01-26'; // дата закінчення відльожки
+  static targetDate = '2025-01-29'; // дата закінчення відльожки
   static bodyLin = 'https://amazing-imperial-bliss.space/ZdKxDCx2'; // тіло лінки з якою будемо працювати
 }
